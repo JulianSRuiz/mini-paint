32d7151a2893ce4207b1fff321d5419673b3f0a4
Julian Ruiz <juliansruiz@gmail.com>
2016-09-01 13:00:32 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  var currentColor = "white";
+
+  $(".box").click(function() {
+    $(this).toggleClass(currentColor);
+  })
+
+  // $(".box").on("dblclick", function() {
+  //   $(this).removeClass(currentColor);
+  // });
+  $('#reset').click(function() {
+    $('.box').removeClass('red blue green yellow white');
+  })
+  $(".color-item").click(function() {
+    currentColor = $(this).text().toLowerCase();
+  });
+});
